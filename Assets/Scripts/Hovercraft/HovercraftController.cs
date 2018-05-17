23ca14cebc5a1647291d68769df27acd9bec1b0f
GVatinno <gianluca.vatinno@gmail.com>
2018-05-17 08:52:34 +0100

    add overcraft propulsors

--- Assets/Scripts/Hovercraft/HovercraftController.cs
+++ Assets/Scripts/Hovercraft/HovercraftController.cs
@@ -7,19 +7,30 @@ public class HovercraftController : MonoBehaviour {
 	private Rigidbody rigidBody;
 	private const float moveForceFactor = 30.0f;
 	private const float turnForceFactor = 0.8f;
-	private const float upForceFactor = 60.0f;
-	private const float maxHeightTotheGround = 3.0f;
+	private const float propellerForceFactor = 60.0f;
+	private const float maxHeightToGround = 3.0f;
+
 
 	private float moveForceMagnitude = 0.0f;
 	private float turnForceMagnitude = 0.0f;
 
-	private int groundLayerMask;
+
+	public float MaxHeightToGround
+	{
+		get { return maxHeightToGround; }
+	}
+
+	public float PropellerForceFactor
+	{
+		get { return propellerForceFactor; }
+	}
+
 
 	// TODO REFACTOR THIS IN SEPARATING INPUT FROM CONTROLLER
 	// TODO USING SCRIPTABLE OBJECT
 
 	void Awake () {
-		groundLayerMask = (1 << LayerMask.NameToLayer("Ground"));
+		
 		rigidBody = GetComponent<Rigidbody> ();
 	}
 
@@ -29,23 +40,6 @@ public class HovercraftController : MonoBehaviour {
 	}
 
 	void FixedUpdate()
-	{
-		ComputeHoverForce ();
-		ComputeMovementForces ();
-	}
-
-	void ComputeHoverForce()
-	{
-		RaycastHit groundHit;
-		Ray ray = new Ray (transform.position, Vector3.down);
-		bool isCollingWithGround = Physics.Raycast (ray, out groundHit, maxHeightTotheGround, groundLayerMask, QueryTriggerInteraction.Ignore);
-		if (isCollingWithGround) {
-			float upForceDistanceFactor = (maxHeightTotheGround - groundHit.distance) / maxHeightTotheGround;
-			rigidBody.AddForce (Vector3.up * upForceDistanceFactor * upForceFactor);
-		}
-	}
-
-	void ComputeMovementForces()
 	{
 		rigidBody.AddRelativeForce (transform.forward * moveForceMagnitude);
 		rigidBody.AddRelativeTorque (transform.up * turnForceMagnitude);
---
+++ Assets/Scripts/Hovercraft/HovercraftPropeller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HovercraftPropeller : MonoBehaviour {
+
+	private Rigidbody rigidBody;
+	private HovercraftController hovercraft;
+	private int groundLayerMask;
+
+
+	void Awake () {
+		
+		hovercraft = GetComponentInParent<HovercraftController> ();
+		rigidBody = hovercraft.GetComponent<Rigidbody> ();
+		groundLayerMask = (1 << LayerMask.NameToLayer("Ground"));
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		RaycastHit groundHit;
+		Ray ray = new Ray (transform.position, Vector3.down);
+		bool isCollingWithGround = Physics.Raycast (ray, out groundHit, hovercraft.MaxHeightToGround, groundLayerMask, QueryTriggerInteraction.Ignore);
+
+		if (isCollingWithGround) {
+			float upForceDistanceFactor = 1.0f - groundHit.distance / hovercraft.MaxHeightToGround;
+			rigidBody.AddForceAtPosition (Vector3.up * upForceDistanceFactor * hovercraft.PropellerForceFactor, transform.position);
+		}
+	}
+}
