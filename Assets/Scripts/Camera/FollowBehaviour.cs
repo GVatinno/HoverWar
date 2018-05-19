da5d9655f3c058c7e6a68a89bd26b0f563a549cf
GVatinno <gianluca.vatinno@gmail.com>
2018-05-19 21:50:38 +0100

    fixed camera and terrain

--- Assets/Scripts/Camera/FollowBehaviour.cs
+++ Assets/Scripts/Camera/FollowBehaviour.cs
@@ -13,17 +13,18 @@ public class FollowBehaviour : MonoBehaviour
 
 	Vector3 m_prevPosition = Vector3.zero;
 	Quaternion m_prevRotation = Quaternion.identity;
+	Vector3 m_velocity = Vector3.zero;
 
 	void LateUpdate() {
 		
-		//this.transform.position = Vector3.SmoothDamp(transform.position, target, ref m_currentVelocity, Time.deltaTime);
 		Vector3 moveBackVector = m_target.transform.rotation * Vector3.up;
+		moveBackVector.Normalize ();
 
 		moveBackVector *= -m_zOffset;
 		moveBackVector.y = -m_yOffset;
 
 		Vector3 target = m_target.transform.position - moveBackVector;
-		this.transform.position = target;
+		this.transform.position = Vector3.SmoothDamp(transform.position, target, ref m_velocity, 0.8f);
 		this.transform.LookAt (m_target.transform);
 
 		if (this.transform.position != m_prevPosition || m_prevRotation != this.transform.rotation)
